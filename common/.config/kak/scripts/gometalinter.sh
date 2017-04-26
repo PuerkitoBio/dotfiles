8c67d59ad4b35401e5612981fb087d3b42087ef1
Martin Angers <martin.n.angers@gmail.com>
2017-04-26 16:26:37 -0400

    fix gometalinter for kakoune

--- common/.config/kak/scripts/gometalinter.sh
+++ common/.config/kak/scripts/gometalinter.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-file=$1
-dir=dirname ${file}
-gometalinter --config=${HOME}/.config/gometalinter/config.json ${dir}
+# gometalinter works with a directory, and reports errors in all files.
+# This script runs in the context of the working directory of the editor,
+# so pass this directory to gometalinter and grep only the issues that
+# relate to the current file.
+
+# BUG: this means that it doesn't work on unsaved buffer.
+
+currentDir=$(pwd)
+currentFile=${kak_buffile}
+
+gometalinter --config=${HOME}/.config/gometalinter/config.json ${currentDir} | grep -e "^${currentFile}:"
 
