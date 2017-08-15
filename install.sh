514508d1a661c11326ddd6418570f26c82cc408d
Martin Angers <martin.n.angers@gmail.com>
2017-08-14 21:28:23 -0400

    make installer work based on distro name, start ubuntu tree

--- install.sh
+++ install.sh
@@ -13,16 +13,26 @@ if [[ ${help} == "-h" ]]; then
 	exit 0
 fi
 
-os=`uname | tr '[:upper:]' '[:lower:]'`
+# ${os} can be:
+# - darwin
+# - ubuntu
+# - arch linux
+# - linux (unknown distribution)
 
-srcdir=`pwd`/${os}
+os=`uname | tr '[:upper:]' '[:lower:]'`
+if [[ ${os} == 'linux' && -f /etc/os-release ]]; then
+	source /etc/os-release
+	os=$(echo ${NAME} | tr '[:upper:]' '[:lower:]')
+fi
+
+srcdir=`pwd`/"${os}"
 commondir=`pwd`/common
 targetdir=${1:-$HOME}
 olddir=${2:-`mktemp -d`}
 
 # remove trailing slash for targetdir and olddir
-targetdir=${targetdir%/}
-olddir=${olddir%/}
+targetdir="${targetdir%/}"
+olddir="${olddir%/}"
 
 if [[ ! -d ${srcdir} ]]; then
 	echo "source directory ${srcdir} does not exist"
@@ -30,7 +40,7 @@ if [[ ! -d ${srcdir} ]]; then
 fi
 
 # create olddir to backup existing files
-mkdir -p ${olddir}
+mkdir -p "${olddir}"
 if [[ $? -ne 0 ]]; then
 	echo "failed to create backup directory ${olddir}"
 	exit 2
@@ -40,20 +50,20 @@ echo "installing to ${targetdir} for OS ${os}"
 echo
 
 # backup existing files, and link new files in target dir
-for filename in {${srcdir},${commondir}}/.*; do
+for filename in {"${srcdir}","${commondir}"}/.*; do
   # consider only regular files (-f)
-	if [[ -f ${filename} ]]; then
-    dir=$(dirname ${filename})
-		filename=$(basename ${filename})
+	if [[ -f "${filename}" ]]; then
+    dir=$(dirname "${filename}")
+		filename=$(basename "${filename}")
 
     # if file is symbolic link (-L) or standard file (exists, -e)
-		if [[ -L ${targetdir}/${filename} || -e ${targetdir}/${filename} ]]; then
+		if [[ -L "${targetdir}/${filename}" || -e "${targetdir}/${filename}" ]]; then
       # backup the existing file to the old directory
-			mv ${targetdir}/${filename} ${olddir}/${filename}
+			mv "${targetdir}/${filename}" "${olddir}/${filename}"
 		fi
 
     # create the symbolic link for the new file to put in place
-		ln -s ${dir}/${filename} ${targetdir}/${filename}
+		ln -s "${dir}/${filename}" "${targetdir}/${filename}"
 		if [[ $? -ne 0 ]]; then
 			echo "failed to symlink ${filename} to ${targetdir}"
 			exit 3
@@ -64,18 +74,18 @@ done
 
 # backup existing config directories, and link new directories in ${targetdir}/.config
 
-if [[ ! -d ${targetdir}/.config ]]; then
+if [[ ! -d "${targetdir}/.config" ]]; then
   mkdir ${targetdir}/.config
 fi
 
-if [[ ! -d ${olddir}/.config ]]; then
+if [[ ! -d "${olddir}/.config" ]]; then
   mkdir ${olddir}/.config
 fi
 
 for dir in ${commondir}/.config/*; do
   # consider only directories (-d)
 	if [[ -d ${dir} ]]; then
-    base=$(basename ${dir})
+    base=$(basename "${dir}")
 
     # ignore . and ..
     if [[ ! ${base} =~ ^\.+$ ]]; then
@@ -83,11 +93,11 @@ for dir in ${commondir}/.config/*; do
       # if directory exists, back it up (symlink or not)
       if [[ -d ${targetdir}/.config/${base} ]]; then
         # backup the existing directory to the old directory
-        mv ${targetdir}/.config/${base} ${olddir}/.config/
+        mv "${targetdir}/.config/${base}" "${olddir}/.config/"
       fi
 
       # create the symbolic link for the directory to put in place
-      ln -s ${dir} ${targetdir}/.config/
+      ln -s "${dir}" "${targetdir}/.config/"
       if [[ $? -ne 0 ]]; then
         echo "failed to symlink ${dir} to ${targetdir}/.config/"
         exit 3
